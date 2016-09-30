6d507fbf4aab6422e4def5cc80c921a660eb82ec
anatom74 <tomaz.kerin@siol.net>
2016-09-30 12:01:40 +0200

    RGB in LedText

--- Test_LED_CTRLR.ino
+++ Test_LED_CTRLR.ino
@@ -24,10 +24,10 @@ void loop()
 {
 	//LED232.println("TEST_COM");
 	//LED232.println(millis());  // Print something via your class.
-	char CC[50] = {};
-	char Cmdout[100] = {};
-	int len;
-	int packetlen;
+//	char CC[50] = {};
+//	char Cmdout[100] = {};/
+//	int len;
+//	int packetlen;
 	char *string;
 	string = "hey";
 	
@@ -37,49 +37,27 @@ void loop()
 	{
 		for (int x = 1; x < 4; x++)
 		{
-			//process_data("TEXTT");
-			delay(1300);
 
-			//LEDPlayProg(0, x, CC, &len);
-			//LEDPackCmd(0xFF, CC, len, Cmdout, &packetlen);
+			delay(500);
+
 			LEDPlayProg_compact(0xFF, 0, x);
-			//SendLEDText232(Cmdout, packetlen);
-			//Serial.println(Cmdout);
+			delay(500);
 
-			delay(1000);
-
-			LEDPlayTxtWindow_compact(0xFF, 0, 0, 5, 100, 3, 2, string);
-						
-		//	LEDPlayTxtWindow(0, 0, 5, 100, 3, 2, string, CC, &len);
-		//	LEDPackCmd(0xFF, CC, len, Cmdout, &packetlen);
-		//	SendLEDText232(Cmdout, packetlen);
+			LEDPlayTxtWindow_compact(0xFF, 0, 0, 5, 100, 3, 2,255,255,255, string);
+		
 		}
 	}
 }
-
-/*void process_data(char *data)
-{
-	//char *buffer = data;
-	Serial.println(strlen(data));
-	for (int i = 0; strlen(data); i++)
-	{
-		Serial.print(*data);
-		data++;
-	}
-
-}*/
 
 
 
 /**
 	Compose CC data for Play desired program on LED screen.
 
+	@ID - id of display (0xFF for any)
 	@option Bit0: Whether to save select play message to flash.    0 not to save，1 save。Bit1~7: Reserved, set to 0
 	@prog_num Number of program to play 1~255 or 0
-	@*CCout output buffer of command
-	@*CCoutlength length of output /CCout) buffer
-
-	note that limit is for 1 program only!!!!
+	note that limit is for 1 program at once only!!!!
 */
 void LEDPlayProg_compact(uint8_t ID, uint8_t option, uint8_t prog_num)  //option
 {
@@ -88,20 +66,99 @@ void LEDPlayProg_compact(uint8_t ID, uint8_t option, uint8_t prog_num)  //option
 	char Cmd_out[100] = {};
 	int paclen;
 
+  //function logic
 	CCout[0] = 0x08;
 	CCout[1] = option;
 	CCout[2] = 0x01;  //1 program only
 	CCout[3] = prog_num;
 	pCCoutlength = 4;
 
-	//	LEDPlayProg(0, x, CC, &len);
-	//	LEDPackCmd(0xFF, CC, len, Cmdout, &packetlen);
+	LEDPackCmd(ID, CCout, pCCoutlength, Cmd_out, &paclen);//pack CMD data
+	SendLEDText232(Cmd_out, paclen); //send CMD data
 
-	LEDPackCmd(ID, CCout, pCCoutlength, Cmd_out, &paclen);
-	SendLEDText232(Cmd_out, paclen);
+}
+
+
+/**
+Compose CC data for: Play text in window CC=0x12
+
+@ID - id of display (0xFF for any)
+@window  - window number 0-7
+@effect - effects for text (0-draw, 1 open from čleft,... se document Special effect for text and picture 
+@alignment - alignment of text (0-2) Bit0~1: the horizontal alignment (
+//////////////0: Left-aligned 
+//////////////1: Horizontal center 
+//////////////2: Right- aligned)
+//////////////Bit2~3: vertical alignment (
+//////////////0:Top-aligned
+//////////////1:Vertically center,
+//////////////2: Bottom- aligned)
+//////////////Bit4~6: Reserved, set to 0
+//////////////Bit7: Reserved, set to 0
+@speed 1-100 (100 fastest)
+@stayTime: seconds
+@font: Bit0~3: font size,see：Font size code 0	8
+//////1	12
+//////2	16
+//////3	24
+//////4	32
+//////5	40
+//////6	48
+//////7	56
+
+//////Bit4~6 :font style,see :Font style code Font style code 	Font style describe
+//////0	Default font style
+//////1	Font style 1
+//////2	Font style 2
+//////3	Font style 3
+//////4	Font style 4
+//////5	Font style 5
+//////6	Font style 6
+//////7	Font style 7
+
+//////Bit7 : reserved (set to 0)
+@Red, Green, Blue: 0-255 color component
+@*Text  - text string to show
+
+*/
+void LEDPlayTxtWindow_compact(uint8_t ID, uint8_t window, uint8_t effect, uint8_t alignment, uint8_t speed, uint16_t stayTime, uint8_t font, uint8_t Red, uint8_t Green, uint8_t Blue, char *Text)  //option
+{
+	
+	char CCout[100] = {};
+	int pCCoutlength;
+	char Cmd_out[100] = {};
+	int paclen;
+
+	int length;
+	char *temp = Text;
+// function logic
+	CCout[0] = 0x12;
+	CCout[1] = window;
+	CCout[2] = effect;  //special effect
+	CCout[3] = alignment;
+	CCout[4] = speed;
+	CCout[5] = highByte(stayTime);
+	CCout[6] = lowByte(stayTime);
+	CCout[7] = font;
+	CCout[8] = Red;  //R 0-255
+	CCout[9] = Green; //G 0-255
+	CCout[10] = Blue; //B
+
+	length = strlen(temp);
+
+	for (int i = 0; i < length; i++)
+	{
+		CCout[11 + i] = Text[i];
+	}
+
+	pCCoutlength = 11 + length;
+
+	LEDPackCmd(ID, CCout, pCCoutlength, Cmd_out, &paclen);//pack CMD data
+	SendLEDText232(Cmd_out, paclen); //send CMD data
 
 
 }
+
 
 /**
 Compose Led command package for sending over serial.
@@ -145,98 +202,6 @@ void LEDPackCmd(uint8_t id, char *CCdata, int CClength, char *outbuffer, int *pB
 	//strcat(LEDcmd, buffer);
 	*pBufflength = count;
 }
-
-
-void LEDPlayProg(uint8_t option, uint8_t prog_num, char *CCout, int *pCCoutlength)  //option
-{
-	CCout[0] = 0x08;
-	CCout[1] = option;
-	CCout[2] = 0x01;  //1 program only
-	CCout[3] = prog_num;
-	*pCCoutlength = 4;
-}
-
-/**
-Compose CC data for: Play text in window CC=0x12
-
-@xx
-@xx
-@xx
-@xx
-*/
-void LEDPlayTxtWindow_compact(uint8_t ID, uint8_t window, uint8_t effect, uint8_t alignment, uint8_t speed, uint16_t stayTime, uint8_t font, char *Text)  //option
-{
-	
-	char CCout[100] = {};
-	int pCCoutlength;
-	char Cmd_out[100] = {};
-	int paclen;
-
-	int length;
-	char *temp = Text;
-
-	CCout[0] = 0x12;
-	CCout[1] = window;
-	CCout[2] = effect;  //special effect
-	CCout[3] = alignment;
-	CCout[4] = speed;
-	CCout[5] = highByte(stayTime);
-	CCout[6] = lowByte(stayTime);
-	CCout[7] = font;
-	CCout[8] = 0xFF;  //R
-	CCout[9] = 0xFF; //G
-	CCout[10] = 0xFF; //B
-
-	length = strlen(temp);
-
-	for (int i = 0; i < length; i++)
-	{
-		CCout[11 + i] = Text[i];
-	}
-
-	pCCoutlength = 11 + length;
-
-	LEDPackCmd(ID, CCout, pCCoutlength, Cmd_out, &paclen);
-	SendLEDText232(Cmd_out, paclen);
-
-
-}
-
-
-/**
-Compose CC data for: Play text in window CC=0x12
-
-@xx
-@xx
-@xx
-@xx
-*/
-void LEDPlayTxtWindow(uint8_t window, uint8_t effect, uint8_t alignment, uint8_t speed, uint16_t stayTime, uint8_t font, char *Text, char *CCout, int *pCCoutlength)  //option
-{
-	int length;
-	char *temp = Text;
-	CCout[0] = 0x12;
-	CCout[1] = window;
-	CCout[2] = effect;  //special effect
-	CCout[3] = alignment;
-	CCout[4] = speed;
-	CCout[5] = highByte(stayTime);
-	CCout[6] = lowByte(stayTime);
-	CCout[7] = font;
-	CCout[8] = 0xFF;  //R
-	CCout[9] = 0xFF; //G
-	CCout[10] = 0xFF; //B
-
-	length = strlen(temp);
-
-	for (int i = 0; i < length;  i++)
-	{
-		CCout[11 + i] = Text[i];
-	}
-
-	*pCCoutlength = 11+length;
-}
-
 
 
 /**
